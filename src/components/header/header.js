796ccfb979ae6067cbaf3fd2fb660977324b2681
TrueBionicle <partem1997@gmail.com>
2023-08-08 08:28:14 +0300

    finalFix

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,7 @@ import "./header.scss";
 const Header = () => {
   return (
     <div className="header">
-      <a href="../app/app.js" className="header-logo">
+      <a href="https://aviasales123456789.surge.sh" className="header-logo">
         <img src={require("./../../assets/img/Logo.jpg")} alt="logo"></img>
       </a>
     </div>
